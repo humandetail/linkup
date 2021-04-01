afaaf6918bab63db04949a6e761a900376a9e914
humandetail.com <512412344@qq.com>
2021-04-01 13:10:43 +0800

    修改压缩插件

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,26 +1,28 @@
 /*
- * @FilePath: \webpack5\webpack.prod.js
+ * @FilePath: \linkup-ts\webpack.prod.js
  * @Description: webpack production config
  * @Author: humandetail
  * @Date: 2021-03-17 00:28:52
  * @LastEditors: humandetail
- * @LastEditTime: 2021-03-18 15:53:49
+ * @LastEditTime: 2021-03-30 16:41:15
  */
 
 const CommonConfig = require('./webpack.common');
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const ClosurePlugin = require('closure-webpack-plugin');
+// const ClosurePlugin = require('closure-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(CommonConfig, {
   mode: 'production',
-  // devtool: 'source-map',
+  devtool: 'source-map',
   optimization: {
     minimizer: [
       new CssMinimizerPlugin(),
-      new ClosurePlugin({
-        mode: 'STANDARD'
-      })
+      // new ClosurePlugin({
+      //   mode: 'STANDARD'
+      // })
+      new TerserPlugin()
     ]
   }
 });
